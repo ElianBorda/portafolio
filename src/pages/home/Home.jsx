cf2966bcf0af4a78c32d3e1c59393aa71e896c39
Elian Borda <eliancamiloalejandro@gmail.com>
2025-03-07 14:18:34 -0300

    transition

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { use, useRef } from 'react'
 import './home.css'
 import vid1 from '../../assets/videos/video1.mp4'
 import vid2 from '../../assets/videos/video2.mp4'
 import vid3 from '../../assets/videos/video3.mp4'
 import VideoCarrousel from '../../assets/components/VideoCarrousel'
 import Loading from '../../assets/components/loading/Loading'
+import { motion, useScroll, useTransform } from 'motion/react'
 
 const Home = () => {
 
-  const videos = {vid1, vid2, vid3}
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"]
+  })
+
+  const scaleY = useTransform(scrollYProgress, [1, 0], [0, 1])
 
   return (
     <>
-      <Loading />
-      <div className='wrapper'>
+      {/* <Loading /> */}
+      <div className='wrapper' >
         <div className='wrapper-intro'>
           <div className='block-vid'>
             {/* <VideoCarrousel videos={videos} /> */}
@@ -26,7 +33,72 @@ const Home = () => {
             <p>Desarrollador profesional</p>
           </div>
         </div>
-        <div className='wrapper-tec'>
+
+        <div className='container-lines'>
+          <motion.div className='line-transition'
+            initial={{
+              scaleY: 0,
+              transformOrigin: 'bottom'
+            }}
+            style={{
+              scaleY: scrollYProgress
+            }}
+            />
+          {/* <motion.div className='line-transition'
+            initial={{
+              scaleY: 0,
+              transformOrigin: 'bottom'
+            }}
+            animate={{
+              scaleY: 1,
+              transition: {
+                delay: 1.2,
+                duration: 0.5
+              }
+            }}
+          />
+          <motion.div className='line-transition'
+            initial={{
+              scaleY: 0,
+              transformOrigin: 'bottom'
+            }}
+            animate={{
+              scaleY: 1,
+              transition: {
+                delay: 1.4,
+                duration: 0.5
+              }
+            }}
+          />
+          <motion.div className='line-transition'
+            initial={{
+              scaleY: 0,
+              transformOrigin: 'bottom'
+            }}
+            animate={{
+              scaleY: 1,
+              transition: {
+                delay: 1.6,
+                duration: 0.5
+              }
+            }}
+          />
+          <motion.div className='line-transition'
+            initial={{
+              scaleY: 0,
+              transformOrigin: 'bottom'
+            }}
+            animate={{
+              scaleY: 1,
+              transition: {
+                delay: 1.8,
+                duration: 0.5
+              }
+            }}
+          /> */}
+        </div>
+
+        <div className='wrapper-tec' useRef={ref}>
           Hola
         </div>
       </div>
